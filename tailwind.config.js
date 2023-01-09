4239f9f9fcb41b3a55009e12d3ffa136b0dae4d9
Huynh Quoc Dung <dunghq4@vingroup.net>
2023-01-09 20:51:43 +0700

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  prefix: '',
+  mode: 'jit',
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: []
+}
