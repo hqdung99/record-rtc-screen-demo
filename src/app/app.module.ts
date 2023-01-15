4c24236cc9cdda7dc6c9eb311d0fff3f08598889
Huynh Quoc Dung <dunghq4@vingroup.net>
2023-01-15 17:58:13 +0700

    add structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './shared/shared.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoRecordingService } from './video-recording.service';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +14,8 @@ import { VideoRecordingService } from './video-recording.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [VideoRecordingService],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import {
+  ButtonComponent
+} from "./components";
+
+@NgModule({
+  imports: [],
+  declarations: [
+    ButtonComponent
+  ],
+  exports: [
+    ButtonComponent
+  ],
+})
+export class SharedModule {}
